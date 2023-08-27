d5b5f5ad53a5c356c5b42cc2fb3bbd18668888f7
Kumari Jyotsna <kumarijyotsna@MacBook-Pro.local>
2023-08-27 17:20:27 +0530

    Binary search recursive and iterative

---
+++ BinarySearchIterative.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+
+public class BinarySearchIterative {
+
+	static int binarySearch(int[] arr, int num, int low, int high) {
+
+		while (high >= low) {
+			int mid = (high+low)/2;
+
+			if (arr[mid] == num) {
+				return mid;
+			}
+
+			if (arr[mid] > num) {
+				high = mid-1;
+			} else {
+				low = mid+1;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = new int[]{3,4, 6, 8, 11, 20};
+		int num1 = 21;
+		int num2 = 8;
+		System.out.println("Index of num " + num1 + " is " + binarySearch(arr, num1, 0, arr.length-1));
+
+		System.out.println("Index of num " + num2 + " is " + binarySearch(arr, num2, 0, arr.length-1));
+
+	}
+}
